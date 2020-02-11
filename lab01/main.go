aa3bfa9419050f209a8aa8e61c77de97a8544549
MokeevDaniil <daniilmokeev13@gmail.com>
2020-02-11 15:32:39 +0300

    lab01 beginning

---
+++ lab01/main.go
@@ -0,0 +1,25 @@
+package main
+import(
+	"fmt"
+	)
+
+type term struct{
+	coef int
+	pow int
+}
+
+func integrate(poly []*term, x0, x int) float{
+	for i, j := range poly{
+		
+	}
+}
+
+func picar(x int)int{
+	u0 := 0
+	poly := make([]*term, 0)
+	poly = append(poly, term{1, 2})
+}
+	
+func main(){
+
+}
